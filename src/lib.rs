715a0e98c632e6725ca602b60e533d26cc17d04b
shashank sv <shashank@shashanks-Laptop.local>
2026-01-03 18:32:51 +0000

    account pda

---
+++ src/lib.rs
@@ -0,0 +1,40 @@
+use anchor_lang::prelude::*;
+
+declare_id!("11111111111111111111111111111111"); // replace later
+
+#[program]
+pub mod agent_registry {
+    use super::*;
+
+    pub fn register_agent(ctx: Context<RegisterAgent>) -> Result<()> {
+        let agent = &mut ctx.accounts.agent;
+
+        agent.agent_pubkey = ctx.accounts.user.key();
+        agent.is_active = true;
+
+        Ok(())
+    }
+}
+
+#[account]
+pub struct Agent {
+    pub agent_pubkey: Pubkey,
+    pub is_active: bool,
+}
+
+#[derive(Accounts)]
+pub struct RegisterAgent<'info> {
+    #[account(
+        init,
+        payer = user,
+        seeds = [b"agent", user.key().as_ref()],
+        bump,
+        space = 8 + 32 + 1
+    )]
+    pub agent: Account<'info, Agent>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
